b1e0b89de23dc29e25a5f4c5c5bef96947259801
Sajib Mia <sajibmia35@gmail.com>
2024-06-23 22:51:18 +0600

    Version: 1.0

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        setcookie($name, $value, time() + 60 * $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data, 200)->cookie($name, $value, time() + 60 * $minutes, $path, $domain, $secure, $httpOnly);
+    }
+}
+
